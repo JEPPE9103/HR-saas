8686b116270dc71a8f9ac73058654a77b6caa5dd
Jesper Persson <jespe9103@gmail.com>
2025-08-26 22:53:22 +0200

    Avoid throwing in getFirebaseApp on SSR to prevent prerender crash

--- src/lib/firebase/client.ts
+++ src/lib/firebase/client.ts
@@ -7,10 +7,6 @@ import { getStorage } from "firebase/storage";
 let app: FirebaseApp | undefined;
 
 export function getFirebaseApp(): FirebaseApp {
-  if (typeof window === "undefined") {
-    // Client SDK only used in the browser for this app
-    throw new Error("getFirebaseApp should be called on the client");
-  }
   if (!app) {
     const cfg = {
       apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
